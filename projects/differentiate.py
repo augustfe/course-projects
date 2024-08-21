99e329327125b892553a1d2aec67f45e16083b65
August Femtehjell <august.femtehjell@gmail.com>
2024-08-21 18:41:53 +0200

    Implemented disc. diff.

--- projects/differentiate.py
+++ projects/differentiate.py
@@ -2,11 +2,20 @@ import numpy as np
 
 
 def differentiate(u: np.ndarray, dt: float) -> np.ndarray:
-    pass
+    du = np.zeros(u.shape)
+    du[0] = (u[1] - u[0]) / dt
+    for i in range(1, len(u) - 1):
+        du[i] = (u[i + 1] - u[i - 1]) / (2 * dt)
+    du[-1] = (u[-1] - u[-2]) / dt
+    return du
 
 
 def differentiate_vector(u: np.ndarray, dt: float) -> np.ndarray:
-    pass
+    du = np.zeros(u.shape)
+    du[0] = (u[1] - u[0]) / dt
+    du[1:-1] = (u[2:] - u[:-2]) / (2 * dt)
+    du[-1] = (u[-1] - u[-2]) / dt
+    return du
 
 
 def test_differentiate() -> None:
